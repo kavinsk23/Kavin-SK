00aaf1b0d23e59c283c1b8d30d4325574a156e3d
kavinsk23 <23.kavinsk@gmail.com>
2024-07-28 21:10:24 +0530

    footer component updated with best practices

--- src/components/landingPageComponents/Footer.js
+++ src/components/landingPageComponents/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
+    // Function to scroll to the top of the page
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
@@ -9,70 +10,103 @@ const Footer = () => {
     };
 
     return (
-        <footer className="">
-            <div className="text-xs bg-primary_1 p-3 sm:flex sm:justify-between sm:p-5 xl:p-7 screen3xl:p-12">
-                <div className="text-xs flex justify-between items-center sm:flex sm:items-center sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl screen3xl:text-4xl">
-                    <h5>© 2024 Kavin Sashantha Kalinga. All Rights Reserved.</h5>
-                    <button
-                        onClick={scrollToTop}
-                        className="bg-primary_1 text-black rounded-full ml-8"
-                        style={{fontSize: '16px'}}>
-
-
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         className="w-auto h-auto bg-primary_1 social-media-icon sm:hidden">
-                        <path fill-rule="evenodd"
+        <footer className="bg-primary_1 text-xs p-3 sm:flex sm:justify-between sm:p-5 xl:p-7 screen3xl:p-12">
+            {/* Footer content container */}
+            <div className="flex justify-between items-center sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl screen3xl:text-4xl">
+                <h5>© 2024 Kavin Sashantha Kalinga. All Rights Reserved.</h5>
+                <button
+                    onClick={scrollToTop}
+                    className="bg-primary_1 text-black rounded-full ml-8"
+                    style={{fontSize: '16px'}}
+                    aria-label="Scroll to top"
+                >
+                    {/* Scroll to top icon for mobile */}
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24" 
+                        fill="currentColor"
+                        className="w-auto h-auto bg-primary_1 social-media-icon sm:hidden"
+                    >
+                        <path fillRule="evenodd"
                               d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
-                              clip-rule="evenodd"/>
+                              clipRule="evenodd"
+                        />
                     </svg>
-                    </button>
-                </div>
+                </button>
+            </div>
 
-                <div className="flex justify-center">
-                    <a className="cursor-pointer"
-                       href="https://www.linkedin.com/in/kavin-sashantha-b92714240?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
-                        <svg className="social-media-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                             viewBox="0 0 24 24">
-                            <path
-                                d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M9,17H6.477v-7H9 V17z M7.694,8.717c-0.771,0-1.286-0.514-1.286-1.2s0.514-1.2,1.371-1.2c0.771,0,1.286,0.514,1.286,1.2S8.551,8.717,7.694,8.717z M18,17h-2.442v-3.826c0-1.058-0.651-1.302-0.895-1.302s-1.058,0.163-1.058,1.302c0,0.163,0,3.826,0,3.826h-2.523v-7h2.523v0.977 C13.93,10.407,14.581,10,15.802,10C17.023,10,18,10.977,18,13.174V17z"></path>
-                        </svg>
-                    </a>
-                    <a className="cursor-pointer"
-                       href="https://github.com/kavinsk23/">
-                        <svg className="social-media-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                             viewBox="0 0 50 50">
-                            <path
-                                d="M17.791,46.836C18.502,46.53,19,45.823,19,45v-5.4c0-0.197,0.016-0.402,0.041-0.61C19.027,38.994,19.014,38.997,19,39 c0,0-3,0-3.6,0c-1.5,0-2.8-0.6-3.4-1.8c-0.7-1.3-1-3.5-2.8-4.7C8.9,32.3,9.1,32,9.7,32c0.6,0.1,1.9,0.9,2.7,2c0.9,1.1,1.8,2,3.4,2 c2.487,0,3.82-0.125,4.622-0.555C21.356,34.056,22.649,33,24,33v-0.025c-5.668-0.182-9.289-2.066-10.975-4.975 c-3.665,0.042-6.856,0.405-8.677,0.707c-0.058-0.327-0.108-0.656-0.151-0.987c1.797-0.296,4.843-0.647,8.345-0.714 c-0.112-0.276-0.209-0.559-0.291-0.849c-3.511-0.178-6.541-0.039-8.187,0.097c-0.02-0.332-0.047-0.663-0.051-0.999 c1.649-0.135,4.597-0.27,8.018-0.111c-0.079-0.5-0.13-1.011-0.13-1.543c0-1.7,0.6-3.5,1.7-5c-0.5-1.7-1.2-5.3,0.2-6.6 c2.7,0,4.6,1.3,5.5,2.1C21,13.4,22.9,13,25,13s4,0.4,5.6,1.1c0.9-0.8,2.8-2.1,5.5-2.1c1.5,1.4,0.7,5,0.2,6.6c1.1,1.5,1.7,3.2,1.6,5 c0,0.484-0.045,0.951-0.11,1.409c3.499-0.172,6.527-0.034,8.204,0.102c-0.002,0.337-0.033,0.666-0.051,0.999 c-1.671-0.138-4.775-0.28-8.359-0.089c-0.089,0.336-0.197,0.663-0.325,0.98c3.546,0.046,6.665,0.389,8.548,0.689 c-0.043,0.332-0.093,0.661-0.151,0.987c-1.912-0.306-5.171-0.664-8.879-0.682C35.112,30.873,31.557,32.75,26,32.969V33 c2.6,0,5,3.9,5,6.6V45c0,0.823,0.498,1.53,1.209,1.836C41.37,43.804,48,35.164,48,25C48,12.318,37.683,2,25,2S2,12.318,2,25 C2,35.164,8.63,43.804,17.791,46.836z"></path>
-                        </svg>
-                    </a>
-                    <a className="cursor-pointer" href="https://www.facebook.com/kaveen.sashantha?mibextid=LQQJ4d">
-                        <svg className="social-media-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                             viewBox="0 0 24 24">
-                            <path
-                                d="M12,2C6.477,2,2,6.477,2,12c0,5.013,3.693,9.153,8.505,9.876V14.65H8.031v-2.629h2.474v-1.749 c0-2.896,1.411-4.167,3.818-4.167c1.153,0,1.762,0.085,2.051,0.124v2.294h-1.642c-1.022,0-1.379,0.969-1.379,2.061v1.437h2.995 l-0.406,2.629h-2.588v7.247C18.235,21.236,22,17.062,22,12C22,6.477,17.523,2,12,2z"></path>
-                        </svg>
-                    </a>
-                    <a className="cursor-pointer"
-                       href="https://www.instagram.com/cheazze_buddy?igsh=dW0xenA4Z216a3Fm&utm_source=qr">
-                        <svg className="social-media-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                             viewBox="0 0 24 24">
-                            <path
-                                d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
-                        </svg>
-                    </a>
-                    <button
-                        onClick={scrollToTop}
-                        className="bg-primary_1 text-black rounded-full ml-8"
-                        style={{fontSize: '16px'}}>
-
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                             className="hidden sm:flex w-auto h-auto bg-primary_1 social-media-icon">
-                            <path fill-rule="evenodd"
-                                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
-                                  clip-rule="evenodd"/>
-                        </svg>
-                    </button>
-                </div>
+            {/* Social media links */}
+            <div className="flex justify-center">
+                <a 
+                    className="cursor-pointer" 
+                    href="https://www.linkedin.com/in/kavin-sashantha-b92714240?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" 
+                    aria-label="LinkedIn"
+                >
+                    <svg 
+                        className="social-media-icon" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24"
+                    >
+                        <path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M9,17H6.477v-7H9 V17z M7.694,8.717c-0.771,0-1.286-0.514-1.286-1.2s0.514-1.2,1.371-1.2c0.771,0,1.286,0.514,1.286,1.2S8.551,8.717,7.694,8.717z M18,17h-2.442v-3.826c0-1.058-0.651-1.302-0.895-1.302s-1.058,0.163-1.058,1.302c0,0.163,0,3.826,0,3.826h-2.523v-7h2.523v0.977 C13.93,10.407,14.581,10,15.802,10C17.023,10,18,10.977,18,13.174V17z"></path>
+                    </svg>
+                </a>
+                <a 
+                    className="cursor-pointer" 
+                    href="https://github.com/kavinsk23/" 
+                    aria-label="GitHub"
+                >
+                    <svg 
+                        className="social-media-icon" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 50 50"
+                    >
+                        <path d="M17.791,46.836C18.502,46.53,19,45.823,19,45v-5.4c0-0.197,0.016-0.402,0.041-0.61C19.027,38.994,19.014,38.997,19,39 c0,0-3,0-3.6,0c-1.5,0-2.8-0.6-3.4-1.8c-0.7-1.3-1-3.5-2.8-4.7C8.9,32.3,9.1,32,9.7,32c0.6,0.1,1.9,0.9,2.7,2 c0.9,1.1,1.8,2,3.4,2 c2.487,0,3.82-0.125,4.622-0.555C21.356,34.056,22.649,33,24,33v-0.025c-5.668-0.182-9.289-2.066-10.975-4.975 c-3.665,0.042-6.856,0.405-8.677,0.707c-0.058-0.327-0.108-0.656-0.151-0.987c1.797-0.296,4.843-0.647,8.345-0.714 c-0.112-0.276-0.209-0.559-0.291-0.849c-3.511-0.178-6.541-0.039-8.187,0.097c-0.02-0.332-0.047-0.663-0.051-0.999 c1.649-0.135,4.597-0.27,8.018-0.111c-0.079-0.5-0.13-1.011-0.13-1.543c0-1.7,0.6-3.5,1.7-5c-0.5-1.7-1.2-5.3,0.2-6.6 c2.7,0,4.6,1.3,5.5,2.1C21,13.4,22.9,13,25,13s4,0.4,5.6,1.1c0.9-0.8,2.8-2.1,5.5-2.1c1.5,1.4,0.7,5,0.2,6.6c1.1,1.5,1.7,3.2,1.6,5 c0,0.484-0.045,0.951-0.11,1.409c3.499-0.172,6.527-0.034,8.204,0.102c-0.002,0.337-0.033,0.666-0.051,0.999 c-1.671-0.138-4.775-0.28-8.359-0.089c-0.089,0.336-0.197,0.663-0.325,0.98c3.546,0.046,6.665,0.389,8.548,0.689 c-0.043,0.332-0.093,0.661-0.151,0.987c-1.912-0.306-5.171-0.664-8.879-0.682C35.112,30.873,31.557,32.75,26,32.969V33 c2.6,0,5,3.9,5,6.6V45c0,0.823,0.498,1.53,1.209,1.836C41.37,43.804,48,35.164,48,25C48,12.318,37.683,2,25,2S2,12.318,2,25 C2,35.164,8.63,43.804,17.791,46.836z"></path>
+                    </svg>
+                </a>
+                <a 
+                    className="cursor-pointer" 
+                    href="https://www.facebook.com/kaveen.sashantha?mibextid=LQQJ4d" 
+                    aria-label="Facebook"
+                >
+                    <svg 
+                        className="social-media-icon" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24"
+                    >
+                        <path d="M12,2C6.477,2,2,6.477,2,12c0,5.013,3.693,9.153,8.505,9.876V14.65H8.031v-2.629h2.474v-1.749 c0-2.896,1.411-4.167,3.818-4.167c1.153,0,1.762,0.085,2.051,0.124v2.294h-1.642c-1.022,0-1.379,0.969-1.379,2.061v1.437h2.995 l-0.406,2.629h-2.588v7.247C18.235,21.236,22,17.062,22,12C22,6.477,17.523,2,12,2z"></path>
+                    </svg>
+                </a>
+                <a 
+                    className="cursor-pointer" 
+                    href="https://www.instagram.com/cheazze_buddy?igsh=dW0xenA4Z216a3Fm&utm_source=qr" 
+                    aria-label="Instagram"
+                >
+                    <svg 
+                        className="social-media-icon" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24"
+                    >
+                        <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
+                    </svg>
+                </a>
+                <button
+                    onClick={scrollToTop}
+                    className="bg-primary_1 text-black rounded-full ml-8 hidden sm:flex"
+                    style={{fontSize: '16px'}}
+                    aria-label="Scroll to top"
+                >
+                    {/* Scroll to top icon for desktop */}
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24" 
+                        fill="currentColor"
+                        className="w-auto h-auto bg-primary_1 social-media-icon"
+                    >
+                        <path fillRule="evenodd"
+                              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
+                              clipRule="evenodd"/>
+                    </svg>
+                </button>
             </div>
         </footer>
     );
--- src/components/landingPageComponents/IndustryFeedbacks.js
+++ src/components/landingPageComponents/IndustryFeedbacks.js
@@ -4,12 +4,13 @@ import 'aos/dist/aos.css';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import imarsha from "../../assets/images/imarsha.png"
-import sanka from "../../assets/images/sanka.png"
-import ushan from "../../assets/images/ushan.png"
-import ruchira from "../../assets/images/ruchira.png"
-import intervest from "../../assets/images/intervest.png"
+import imarsha from "../../assets/images/imarsha.png";
+import sanka from "../../assets/images/sanka.png";
+import ushan from "../../assets/images/ushan.png";
+import ruchira from "../../assets/images/ruchira.png";
+import intervest from "../../assets/images/intervest.png";
 
+// Testimonials data
 const testimonials = [
     {
         id: 1,
@@ -20,24 +21,21 @@ const testimonials = [
     {
         id: 2,
         image: sanka,
-        text: 'Kavin\'s exceptional skills and positive attitudes made every singe day a success within the team, and his dedication to excellence was truly inspiring and amazing.',
+        text: 'Kavin\'s exceptional skills and positive attitudes made every single day a success within the team, and his dedication to excellence was truly inspiring and amazing.',
         author: 'Sanka - Associate Architect (Intervest Software Technologies)',
     },
-
     {
         id: 4,
         image: ruchira,
-        text: 'Working with Kavin has been a pleasure; his willingness to learn and his team spirit truly stand out. Keep it up, Kavin!.',
+        text: 'Working with Kavin has been a pleasure; his willingness to learn and his team spirit truly stand out. Keep it up, Kavin!',
         author: 'Ruchira - Tech Lead (Intervest Software Technologies)',
     },
-
     {
         id: 3,
         image: ushan,
         text: 'Kavin\'s outstanding performance and collaborative approach were instrumental in achieving our project goals, making our working experience truly remarkable.',
         author: 'Ushan - Senior QA Engineer (Intervest Software Technologies)',
     },
-
     {
         id: 5,
         image: intervest,
@@ -65,35 +63,70 @@ const TestimonialCarousel = () => {
     }, []);
 
     return (
-        <Slider {...settings}>
-            {testimonials.map((testimonial) => (
-                <div key={testimonial.id} className="feedback-slider bg-primary_2 p-6 flex flex-col justify-center items-center lg:flex-row lg:py-14">
-                    <div data-aos="fade-up" className='flex flex-col justify-center items-center lg:flex-row'>
-                        <div className="flex justify-center items-center">
-                            <img className="p-5 max-w-80 sm:max-w-96 lg:max-w-w108 2xl:max-w-w120 screen3xl:max-w-w135" src={testimonial.image} alt=""/>
+        <section aria-labelledby="testimonials">
+            <Slider {...settings}>
+                {testimonials.map((testimonial) => (
+                    <article 
+                        key={testimonial.id} 
+                        className="feedback-slider bg-primary_2 p-6 flex flex-col justify-center items-center lg:flex-row lg:py-14" 
+                        data-aos="fade-up"
+                    >
+                        <div className='flex flex-col justify-center items-center lg:flex-row'>
+                            
+                            {/* Testimonial Image */}
+                            <div className="flex justify-center items-center">
+                                <img 
+                                    className="p-5 max-w-80 sm:max-w-96 lg:max-w-w108 2xl:max-w-w120 screen3xl:max-w-w135" 
+                                    src={testimonial.image} 
+                                    alt={`Photo of ${testimonial.author}`} 
+                                />
+                            </div>
+                            
+                            {/* Testimonial Content */}
+                            <div className="bg-secondary-100 p-2 rounded-md text-xs sm:text-sm md:max-w-w135 md:text-base lg:text-xl lg:max-w-w108 lg:py-10 lg:px-5 xl:text-2xl xl:py-20 2xl:max-w-w120 2xl:py-30 2xl:leading-10 screen3xl:max-w-w135 screen3xl:text-3xl screen3xl:py-32 screen3xl:leading-leading15">
+                                <div>
+                                    <svg 
+                                        className="rotate-180 flex justify-start" 
+                                        width="15" 
+                                        height="30" 
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <text 
+                                            x="0" 
+                                            y="24" 
+                                            fontSize="35" 
+                                            fill="#D9C5A6"
+                                        >,,
+                                        </text>
+                                    </svg>
+                                </div>
+                                <div className="text-white leading-8 2xl:leading-10 screen3xl:leading-leading15">
+                                    {testimonial.text}
+                                </div>
+                                <div className="flex justify-end">
+                                    <svg 
+                                        width="15" 
+                                        height="30" 
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <text 
+                                            x="0" 
+                                            y="24" 
+                                            fontSize="35" 
+                                            fill="#D9C5A6"
+                                        >,,
+                                        </text>
+                                    </svg>
+                                </div>
+                                <div className="text-gray-400 pt-2">
+                                    {testimonial.author}
+                                </div>
+                            </div>
                         </div>
-                        <div className="bg-secondary-100 p-2 rounded-md text-xs sm:text-sm md:max-w-w135 md:text-base lg:text-xl lg:max-w-w108 lg:py-10 lg:px-5 xl:text-2xl xl:py-20 2xl:max-w-w120 2xl:py-30 2xl:leading-10 screen3xl:max-w-w135 screen3xl:text-3xl screen3xl:py-32 screen3xl:leading-leading15">
-                            <div>
-                                <svg className="rotate-180 flex justify-start" width="15" height="30" xmlns="http://www.w3.org/2000/svg">
-                                    <text x="0" y="24" font-size="35" fill="#D9C5A6">,,</text>
-                                </svg>
-                            </div>
-                            <div className="text-white leading-8 2xl:leading-10 screen3xl:leading-leading15">
-                                {testimonial.text}
-                            </div>
-                            <div className="flex justify-end">
-                                <svg width="15" height="30" xmlns="http://www.w3.org/2000/svg">
-                                    <text x="0" y="24" font-size="35" fill="#D9C5A6">,,</text>
-                                </svg>
-                            </div>
-                            <div className="text-gray-400 pt-2">
-                                {testimonial.author}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </Slider>
+                    </article>
+                ))}
+            </Slider>
+        </section>
     );
 };
 
