4e1c129be48539956d19c1e00891aaeaa8732363
kavinsk23 <23.kavinsk@gmail.com>
2024-07-28 20:29:35 +0530

    skills component updated with best practices

--- src/components/landingPageComponents/Skills.js
+++ src/components/landingPageComponents/Skills.js
@@ -1,67 +1,121 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import bootstrap from "../../assets/images/bootstrap.png"
-import css from "../../assets/images/css.png"
-import enonic from "../../assets/images/enonic.png"
-import figma from "../../assets/images/figma.png"
-import git from "../../assets/images/git.png"
-import html from "../../assets/images/html.png"
-import jira from "../../assets/images/jira.png"
-import js from "../../assets/images/js.png"
-import Optimizely from "../../assets/images/Optimizely.png"
-import postman from "../../assets/images/postman.png"
-import react from "../../assets/images/react.png"
-import redmine from "../../assets/images/redmine.png"
-import sass from "../../assets/images/sass.png"
-import tailwind from "../../assets/images/tailwind.png"
-import browserstack from "../../assets/images/browserstack.png"
-import angular from "../../assets/images/angular.png"
-import mui from "../../assets/images/mui.png"
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function Skills() {
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false); // State for mobile menu toggle
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        AOS.init({
-            duration: 1200, // Animation duration
-        });
-    }, []);
+    // Function to navigate to the home page
+    const handleNavigate = () => {
+        navigate('/');
+    };
 
     return (
-        <div className="bg-primary_1 w-full h-auto" id="skills">
-            <div data-aos="fade-up">
-                <div className="skill-icons-div">
-                    <img className="skill-icons" src={html} alt=""/>
-                    <img className="skill-icons" src={css} alt=""/>
-                </div>
+        <nav className="font-comfortaa bg-secondary-100 text-primary_1 sm:bg-primary_1 sm:text-black">
+            <div className="container mx-auto px-4 text-xs font-thin sm:justify-between sm:px-4 sm:py-4 sm:text-sm md:text-base md:w-26 lg:text-lg lg:py-6 lg:font-medium xl:text-2xl xl:py-10 xl:px-16 screen3xl:text-4xl">
+                <div className="flex justify-between items-center py-5">
+                    {/* Mobile "Kavin SK" button */}
+                    <button 
+                        className="text-xl font-bold flex-1 sm:hidden cursor-pointer"
+                        onClick={handleNavigate}
+                    >
+                        Kavin SK
+                    </button>
 
-                <div className="skill-icons-div">
-                    <img className="skill-icons" src={react} alt=""/>
-                    <img className="skill-icons" src={angular} alt=""/>
-                    <img className="skill-icons" src={tailwind} alt=""/>
-                    <img className="skill-icons" src={sass} alt=""/>
-                </div>
+                    {/* Left side links for larger screens */}
+                    <div className="hidden sm:flex flex-1 justify-start items-center">
+                        <a 
+                            href="#about" 
+                            className="py-2 px-3 md:min-w-26 xl:px-8 2xl:px-12"
+                        >
+                            About me
+                        </a>
+                        <a 
+                            href="#skills" 
+                            className="py-2 px-3 md:min-w-26 xl:px-8 2xl:px-12"
+                        >
+                            Skills
+                        </a>
+                    </div>
+                    
+                    {/* "Kavin SK" button for larger screens, centered */}
+                    <button 
+                        className="nav-buttons font-bold sm:text-2xl sm:w-40 sm:mx-16 md:text-3xl md:w-64 md:px-10 lg:text-4xl lg:w-96 lg:px-16 xl:text-5xl xl:w-96 xl:px-14 font-fra screen3xl:text-7xl screen3xl:w-w108 cursor-pointer"
+                        onClick={handleNavigate}
+                    >
+                        Kavin SK
+                    </button>
 
-                <div className="skill-icons-div">
-                    <img className="skill-icons" src={bootstrap} alt=""/>
-                    <img className="skill-icons" src={js} alt=""/>
-                    <img className="skill-icons" src={enonic} alt=""/>
-                    <img className="skill-icons" src={figma} alt=""/>
-                    <img className="skill-icons" src={postman} alt=""/>
-                </div>
+                    {/* Right side links for larger screens */}
+                    <div className="hidden sm:flex flex-1 justify-end items-center">
+                        <a 
+                            href="#education" 
+                            className="py-2 px-3 md:min-w-26 xl:px-8 2xl:px-12"
+                        >
+                            Education
+                        </a>
+                        <a 
+                            href="#portfolio" 
+                            className="py-2 px-3 md:min-w-26 xl:px-8 2xl:px-12"
+                        >
+                            Portfolio
+                        </a>
+                    </div>
 
-                <div className="skill-icons-div">
-                    <img className="skill-icons" src={browserstack} alt=""/>
-                    <img className="skill-icons" src={Optimizely} alt=""/>
-                    <img className="skill-icons" src={git} alt=""/>
-                    <img className="skill-icons" src={mui} alt=""/>
-                </div>
-
-                <div className="skill-icons-div">
-                    <img className="skill-icons" src={redmine} alt=""/>
-                    <img className="skill-icons" src={jira} alt=""/>
+                    {/* Mobile menu button */}
+                    <div className="sm:hidden flex items-center">
+                        <button 
+                            onClick={() => setIsOpen(!isOpen)} 
+                            className="ml-auto"
+                        >
+                            <svg 
+                                className="w-6 h-6" 
+                                fill="none" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round" 
+                                strokeWidth={2} 
+                                viewBox="0 0 24 24" 
+                                stroke="currentColor"
+                            >
+                                {isOpen ? (
+                                    <path d="M6 18L18 6M6 6l12 12"></path>
+                                ) : (
+                                    <path d="M4 6h16M4 12h16m-4 6h4"></path>
+                                )}
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+            {/* Mobile Menu, expanded/collapsed based on 'isOpen' state */}
+            <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`}>
+                <a 
+                    href="#about" 
+                    className="block py-2 px-4 text-sm"
+                >
+                    About me
+                </a>
+                <a 
+                    href="#skills" 
+                    className="block py-2 px-4 text-sm"
+                >
+                    Skills
+                </a>
+                <a 
+                    href="#education" 
+                    className="block py-2 px-4 text-sm"
+                >
+                    Education
+                </a>
+                <a 
+                    href="#portfolio" 
+                    className="block py-2 px-4 text-sm"
+                >
+                    Portfolio
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
