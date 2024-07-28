0098dacce6c6b2e65f45559db3f621213403a0e9
kavinsk23 <23.kavinsk@gmail.com>
2024-07-28 21:00:05 +0530

    achievements component updated with best practices

--- src/components/landingPageComponents/Achievements.js
+++ src/components/landingPageComponents/Achievements.js
@@ -3,29 +3,38 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 export default function Achievements() {
-
+  
+    // Initialize AOS (Animate on Scroll) library
     useEffect(() => {
         AOS.init({
             duration: 1200, // Animation duration
         });
     }, []);
     
-    return(
-        <div className="bg-primary_1 justify-evenly py-5 xl:py-10">
-            <div className="flex bg-primary_1 justify-evenly py-5 xl:py-10" data-aos="fade-up">
-                <div className="achievements-div">
-                    <h1 className="achievements-num">10+</h1>
-                    <h3 className="achievements-text">Self Projects</h3>
-                </div>
-                <div className="achievements-div">
-                    <h1 className="achievements-num">3</h1>
-                    <h3 className="achievements-text">Advanced Certificates</h3>
-                </div>
-                <div className="achievements-div">
-                    <h1 className="achievements-num">12+</h1>
-                    <h3 className="achievements-text">Months Experience</h3>
-                </div>
+    return (
+        <section className="bg-primary_1 py-5 xl:py-10">
+            <div 
+                className="flex justify-evenly py-5 xl:py-10" 
+                data-aos="fade-up">
+                
+                    {/* Achievement 1 */}
+                    <div className="achievements-div">
+                        <h1 className="achievements-num">10+</h1>
+                        <h3 className="achievements-text">Self Projects</h3>
+                    </div>
+                    
+                    {/* Achievement 2 */}
+                    <div className="achievements-div">
+                        <h1 className="achievements-num">3</h1>
+                        <h3 className="achievements-text">Advanced Certificates</h3>
+                    </div>
+                    
+                    {/* Achievement 3 */}
+                    <div className="achievements-div">
+                        <h1 className="achievements-num">12+</h1>
+                        <h3 className="achievements-text">Months Experience</h3>
+                    </div>
             </div>
-        </div>
-    )
+        </section>
+    );
 }
